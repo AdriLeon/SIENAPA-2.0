3ce7027f9b651cc8a8dd20a9b1ecea643a6ec60e
luisgar289 <1720110205@utectulancingo.edu.mx>
2023-10-22 00:21:35 -0600

    Login Funcional

--- sienapa_movil/lib/UI/login.dart
+++ sienapa_movil/lib/UI/login.dart
@@ -1,89 +1,117 @@
 import 'package:flutter/material.dart';
-import 'package:sienapa_movil/UI/lista_pozos.dart';
+import 'package:get/get.dart';
+import 'package:sienapa_movil/Constants/auth_constants.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+import 'package:sienapa_movil/Controller/auth_controller.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffE5E5E5),
+        backgroundColor: const Color(0xffE5E5E5),
         body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 50,),
-                Image(image: AssetImage('assets/sienapa.png'), height: 150, width: 150,),
-                Padding(padding: EdgeInsets.all(16),
-                  child: Form(
-                    // key: controller.formKey,
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          'Inicia Sesión',
-                          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
-                          ,
-                        ),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        TextFormField(
-                          // validator: controller.emailValidator,
-                          keyboardType: TextInputType.emailAddress,
-                          // controller: controller.emailController,
-                          decoration: InputDecoration(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Image(
+                image: AssetImage('assets/sienapa.png'),
+                height: 150,
+                width: 150,
+              ),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        'Inicia Sesion',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _emailController,
+                        decoration: InputDecoration(
                             hintText: 'Email',
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          //TODO: Eliminar botton de registro
+                          ElevatedButton(
+                            onPressed: () async {
+                              authController.emailRegister(
+                                  _emailController.text.trim(),
+                                  _passwordController.text.trim());
+                            },
+                            child: const Text('Registrarse'),
                           ),
-                          // onSaved: (value) {
-                          //   controller.email = value! as RxString;
-                          // },
-                        ),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        TextFormField(
-                          // validator: controller.passwordValidator,
-                          obscureText: true,
-                          // controller: controller.passwordController,
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          // onSaved: (value) {
-                          //   controller.password = value! as RxString;
-                          // },
-                        ),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        Container(
-                          height: 50,
-                          width: 150,
-                          child: MaterialButton(onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => ListaPozos() ));
-                          },
-                            color: const Color(0xff599EAC),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Text('Ingresar', style: TextStyle(color: Colors.white, fontSize: 20),),
-                          ),
-                        )
-                      ],
-                    ),
+                          Padding(padding: EdgeInsets.all(10)),
+                          ElevatedButton(
+                              onPressed: () async {
+                                authController.emailLogin(
+                                    _emailController.text.trim(),
+                                    _passwordController.text.trim());
+                              },
+                              child: const Text('Iniciar Sesion'))
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                          child: SignInButton(
+                        Buttons.google,
+                        text: "Iniciar Sesion con Google",
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: EdgeInsets.all(5),
+                        onPressed: () async {
+                          authController.signInWithGoogle();
+                        },
+                      ))
+                    ],
                   ),
                 ),
-              ],
-            )
+              ),
+            ],
+          ),
         ));
-    //Center(child: Text(Get.find<LoginPageController>().title.value)));
   }
 }
